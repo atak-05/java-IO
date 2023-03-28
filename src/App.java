99b37b49934385dd214d03c584d4cb0159ed07f4
gizem <gizemtuncer17@gmail.com>
2023-03-28 16:55:28 +0300

    File I/Owas working

---
+++ src/App.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        File file = new File("lib/text.txt");        
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("Hello world");
+        bw.newLine();
+        bw.write("Hello Java");
+        bw.close();
+
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String content ="";        
+        String line = br.readLine();
+
+        while(line!=null){
+            content+=line+System.lineSeparator();
+            line=br.readLine();
+        }
+        System.out.println(content);
+        br.close();
+
+    }   
+    
+    
+    
+    
+}
